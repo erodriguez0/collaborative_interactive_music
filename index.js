dbbdafa6a6f882e94f977f0cb3f9e09b71433425
Mattrg <Mattrg@users.noreply.github.com>
2019-12-01 20:25:35 -0800

    changed index.js

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ synth.triggerAttack('C4','8n');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+//app.get('/', function(request, response) {
+//  response.send('Hello World!')
+//})
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 app.get("/chat", function(req,res){
   res.sendFile(path + "chat.html");
