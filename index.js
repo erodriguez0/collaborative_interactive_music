29e5d923d3090bc5e3465e9dec749dd844c7491c
Mattrg <Mattrg@users.noreply.github.com>
2019-12-02 16:24:27 -0800

    reverted changes

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ synth.triggerAttack('C4','8n');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-//app.get('/', function(request, response) {
-//  response.send('Hello World!')
-//})
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+
 
 app.get("/chat", function(req,res){
   res.sendFile(path + "chat.html");
@@ -38,4 +36,3 @@ io.on('connection', function(socket){
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-
